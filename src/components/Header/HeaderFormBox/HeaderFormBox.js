a91e9d85df520bfbd98c2fd186b50ce46d10f489
cuiqiusheng <363092524@qq.com>
2016-10-28 17:46:50 +0800

    git add .

--- src/components/Header/HeaderFormBox/HeaderFormBox.js
+++ src/components/Header/HeaderFormBox/HeaderFormBox.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import HeaderForm from  './HeaderForm/HeaderForm';
+import HeaderForm from  './HeaderForm';
 
 class HeaderFormBox extends React.Component{
     render(){
